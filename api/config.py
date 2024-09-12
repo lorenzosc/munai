a520432b9627985b3f2f54f9fae3d06a7a94378e
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-12 01:25:05 -0300

    feat: create file for config class

---
+++ api/config.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///patients.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+    APP_NAME = os.getenv('APP_NAME', 'PatientDataAPI')
+    TEMP_DIR = os.getenv('TEMP_DIR', './temp_storage')
