0d2fc278010bcf98722b4e5baaa1a146b5e18c72
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-13 08:56:07 -0300

    fix: Correct environment variables

--- api/config.py
+++ api/config.py
@@ -7,8 +7,8 @@ class Config:
     SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///patients.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-    result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+    broker_url = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+    result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
     FHIR_SERVER_URL = os.getenv('FHIR_SERVER_URL', 'http://localhost:8080/baseR4')
     APP_NAME = os.getenv('APP_NAME', 'PatientDataAPI')
     TEMP_DIR = os.getenv('TEMP_DIR', './temp_storage')
