6212a72688cfd0d5f17595d1bf64c1d070f99f40
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-13 13:54:04 -0300

    fix: adjust db access

--- api/config.py
+++ api/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///./dev.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:////app/db/test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     broker_url = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
     result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
