61844d408f8454550ae8e81aadb9a806e274fb1a
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-13 08:55:32 -0300

    fix: Initiate database in app

--- api/app.py
+++ api/app.py
@@ -13,10 +13,12 @@ app.config.from_object(Config)
 
 jwt = JWTManager(app)
 
+init_app(app)
+
+db.init_app(app)
+
 with app.app_context():
     db.create_all()
 
-init_app(app)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
